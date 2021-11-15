064be8b52451ea5e0590471efa89ea6595129866
kneskoromny <kneskoromny@gmail.com>
2021-11-15 10:48:32 +0300

    Initial Commit

---
+++ TestTask_CreateScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask_CreateScreen
+//
+//  Created by Кирилл Нескоромный on 15.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
